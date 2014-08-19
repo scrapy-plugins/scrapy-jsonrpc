d3bd70e58fedfbdde9daa1a401e0bf7a88536d4d
Daniel Graña <dangra@gmail.com>
2014-08-19 11:37:39 -0300

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrapy-jsonrpc',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/scrapy/scrapy-jsonrpc',
     description='Scrapy extenstion to control spiders using JSON-RPC',
     author='Scrapy developers',
